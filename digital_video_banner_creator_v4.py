c8a511fa36e924b2e417bf789b1122163b6249d9
Wong Jun Kang <88149192+JunKangWong@users.noreply.github.com>
2024-05-04 20:20:17 +0800

    added sample config

--- digital_video_banner_creator_v4.py
+++ digital_video_banner_creator_v4.py
@@ -1,22 +1,38 @@
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
 # Load your video
-video = VideoFileClip("/Users/junkangwong/Documents/github_repo/digital_card/input/sample_vid2.mp4")
+video = VideoFileClip("/Users/junkangwong/Documents/github_repo/digital_card/input/sample_vid3.mp4")
 
+# TODO:  Make all dynamic values parameterized with same variable
+# Attendees name
 # Create a text clip. The text clip has transparent background by default.
-text = TextClip("Hello, World!", fontsize=70, color='black', font='Amiri-Bold', size=video.size)
+text = TextClip("Patricia Thomas", fontsize=32, color='black', font='./font/BASKVILL.ttf', size=video.size)
 
 # Set duration for the text and its position
-text = text.set_duration(9).set_position("center")
+text = text.set_duration(9.5).set_position(("center", -145))
 
 # Apply fade-in and fade-out effects (fading from transparent to white and then to black)
 text = text.crossfadein(1)
 
 # Set the text to appear at a specific time
-text = text.set_start(1)
+text = text.set_start(0.5)
+
+# Table Number
+# Create a text clip. The text clip has transparent background by default.
+table_no = TextClip("Table No: T1", fontsize=18, color='black', font='./font/BASKVILL.ttf', size=video.size)
+
+# Set duration for the text and its position
+table_no = table_no.set_duration(9.5).set_position(("center", -110))
+
+# Apply fade-in and fade-out effects (fading from transparent to white and then to black)
+table_no = table_no.crossfadein(1)
+
+# Set the text to appear at a specific time
+table_no = table_no.set_start(0.5) 
+
 
 # Composite the text onto the video
-final_video = CompositeVideoClip([video, text], use_bgclip=True)
+final_video = CompositeVideoClip([video, text, table_no], use_bgclip=True)
 
 # Write the result to a file
 final_video.write_videofile("/Users/junkangwong/Documents/github_repo/digital_card/output/output_sample.mp4", codec='libx264', bitrate="5000k")
